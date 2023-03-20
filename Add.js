b068ccb73a67019fa648509707a0a828582ea949
Nada-Abdelmohsen <61163621+Nada-abdelmohsen@users.noreply.github.com>
2023-03-20 20:19:42 +0200

    Delete Add.js

--- Add.js
+++
@@ -1,9 +0,0 @@
-function Add(x,y){
-    if(typeof x=='number'&&typeof y=='number')
-        console.log("Add: ",x+y);
-    else
-        console.log("Please enter numbers only!");
-}  
-module.exports=Add;
-
-

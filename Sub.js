23a1eca4c2fc07508527b0972e9976c6fea1db4f
Nada-Abdelmohsen <61163621+Nada-abdelmohsen@users.noreply.github.com>
2023-03-20 20:20:08 +0200

    Delete Sub.js

--- Sub.js
+++
@@ -1,7 +0,0 @@
-function Sub(x,y){
-    if(typeof x=='number'&& typeof y=='number')
-        console.log("Sub: "+(x-y));
-   else
-        console.log("Please enter numbers only!");
-}
-module.exports=Sub;
